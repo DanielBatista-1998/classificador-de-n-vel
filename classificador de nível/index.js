5603ccdb9ca4e6da72709505ac735a66477bf95d
DanielBatista-1998 <danielbalves09@gmail.com>
2024-11-26 13:05:29 -0300

    Add files via upload

---
+++ classificador de nível/index.js
@@ -0,0 +1,28 @@
+let nivelDeXp = 11000
+
+if(nivelDeXp === 1000){
+console.log("O herói de nome Daniel está no nível de Ferro")
+}
+if(nivelDeXp === 2000){
+console.log("O herói de nome Daniel está no nível de Bronze")
+}
+if(nivelDeXp === 5000){
+console.log("O herói de nome Daniel está no nível de Prata")
+}
+if(nivelDeXp === 7000){
+console.log("O herói de nome Daniel está no nível de Ouro")
+}
+if(nivelDeXp === 8000){
+console.log("O herói de nome Daniel está no nível de Platina")
+}
+if(nivelDeXp === 9000){
+console.log("O herói de nome Daniel está no nível de Ascendente")
+}
+if(nivelDeXp === 10000){
+console.log("O herói de nome Daniel está no nível de Imortal")
+}
+if(nivelDeXp  >10000){
+console.log("O herói de nome Daniel está no nível de Radiante")
+}
+
+
